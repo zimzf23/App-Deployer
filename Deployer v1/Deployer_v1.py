8cd874c2280f398d8a8f6d0a65f8823b87157e0d
Arnau <zimzf23@gmail.com>
2025-09-23 14:52:45 +0200

    hello

--- Deployer v1/Deployer_v1.py
+++ Deployer v1/Deployer_v1.py
@@ -1,1 +1,5 @@
+from dependencies import *
 
+ui.label('hello world')
+
+ui.run(title='App Deployer',host='0.0.0.0', port=8080, reload=True, native=True ,storage_secret="asdf"  )
---
+++ Deployer v1/dependencies.py
@@ -0,0 +1,8 @@
+from nicegui import ui, app
+import os
+import subprocess
+import sys
+import threading
+import time
+import platform
+import shutil
